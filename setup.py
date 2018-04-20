a499032ecd1aa453716e002572616e8b95191c96
alex <alex@fabricadigital.com.br>
2018-04-19 22:22:19 -0300

    Add setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rpcs3-update',
+    version='1.0',
+    url='https://github.com/alexsilva/rpcs3-update',
+    license='MIT',
+    author='alex',
+    author_email='geniofuturo@gmail.com',
+    description='rpcs3 emulator update',
+    install_requires=['requests'],
+    py_modules=['rpcs3'],
+)
